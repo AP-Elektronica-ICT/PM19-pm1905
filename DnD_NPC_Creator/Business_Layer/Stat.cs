37d5111f31bce3407ddbf370c775dc7fe590a58d
Zeno <s102215@ap.be>
2019-10-17 11:42:53 +0200

    Stat class added some fields
    Added fields:
    -Name
    -TotalStat
    -StatModifier(readonly)

---
+++ DnD_NPC_Creator/Business_Layer/Stat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Layer
+{
+    class Stat
+    {
+        public string Name;
+        public int TotalStat;
+        public int StatModifier
+        {
+            get
+            {
+                return (int)Math.Ceiling(((decimal)TotalStat - 10m) / 2);
+            }
+        }
+    }
+}
