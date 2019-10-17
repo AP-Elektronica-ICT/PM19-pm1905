833e5ed533082bc7b4d73b4956ce6d6292842154
Zeno <s102215@ap.be>
2019-10-17 11:43:59 +0200

    Added functionality to the skill class
    Basic fields and set/get function (needs to be expanded)

---
+++ DnD_NPC_Creator/Business_Layer/Skill.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Layer
+{
+    class Skill
+    {
+        private int _bonus;
+        private int _proficiencyBonus;
+
+        public string Name;
+        private bool _hasProficiency;
+        private bool _hasExpertice;
+        public string RelatedStat;
+
+        public int getBonus()
+        {
+            return _bonus;
+        } 
+        public void setProficiencyBonus(int proficiencyBonus)
+        {
+            _proficiencyBonus = proficiencyBonus;
+        }
+        public void setExpertise(bool hasExpertise)
+        {
+            _hasExpertice = hasExpertise;
+        }
+    }
+}
